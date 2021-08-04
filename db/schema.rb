d4194e44d8e52f1b59fa1c485a380d5504b02dd2
Matthew Damiata <mdamiat1@binghamton.edu>
2021-08-04 14:20:23 -0400

    ready for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210612004519) do
+ActiveRecord::Schema.define(version: 20210724161428) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "authorizations", force: :cascade do |t|
     t.string   "provider"
