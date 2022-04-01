699e07f788e9e8c78e388a0cc155dd826872a51c
Yash Mittal <yashmittal@Yashs-MacBook-Pro.local>
2022-04-01 11:46:20 -0400

    view users last name update

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -129,6 +129,14 @@ class TimetablesController < ApplicationController
 			end
 	  end
 		non_nil = non_nil.sort_by { |prof| prof.user.name.split(' ').last }
+
+    @profiles.each do |prof|
+      names = prof.user.name.split
+      firstname = names.first
+      lastname = names.last
+      prof.user.name = lastname + ' ' + firstname
+    end
+
 		@profiles = non_nil + is_nil
     @users = Array.new
     Profile.all.each do |x|
