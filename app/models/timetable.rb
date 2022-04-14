d5f82d0fe2afa05f1d1b856b500ca0c3ac2a4e30
Yash Mittal <yashmittal@Yashs-MacBook-Pro.local>
2022-04-14 10:24:22 -0400

    time bug fix

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,10 +1,12 @@
 class Timetable < ActiveRecord::Base
 
 	def self.filter_dates(start_date, end_date, myid)
+		Time.zone = 'Eastern Time (US & Canada)'
 		Timetable.where({:user_id => myid, time_in:start_date..(end_date + 1.day) }).order('time_in ASC')
 	end
 
 	def self.get_user_timetables(myid)
+		Time.zone = 'Eastern Time (US & Canada)'
 		Timetable.where({:user_id => myid}).order('time_in ASC')
 	end
 
